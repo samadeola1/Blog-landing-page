36cc902020a7767bfa75412c511a958d2a2934f2
Izzy-World <umezurikeisrael012@gmail.com>
2025-06-09 09:08:21 -0700

    updated section 3

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -10,18 +10,18 @@ const SimpleSection = () => {
             <img 
               src={illustrationLaptopDesktop} 
               alt="Laptop illustration" 
-              className="hidden md:block w-full max-w-md mx-auto"
+              className="hidden md:block w-full max-w-[700px] -left-40 relative"
             />
             <img 
               src={illustrationLaptopMobile} 
               alt="Laptop illustration" 
-              className="md:hidden w-full max-w-md mx-auto"
+              className="md:hidden max-w-2xl block mx-auto mt-8"
             />
           </div>
           
-          <div className="md:w-1/2 md:pl-16 mt-16 md:mt-0">
+          <div className="md:w-1/2 md:pl-16 mt-16 md:mt-0 md:pr-40 ">
             <div className="mb-12">
-              <h3 className="text-2xl font-bold text-very-dark-blue mb-4">
+              <h3 className="text-2xl font-bold text-very-dark-blue mb-4 pl-[30px] md:pl-0">
                 Free, open, simple
               </h3>
               <p className="text-very-dark-grayish-blue">
@@ -32,7 +32,7 @@ const SimpleSection = () => {
             </div>
             
             <div>
-              <h3 className="text-2xl font-bold text-very-dark-blue mb-4">
+              <h3 className="text-2xl font-bold text-very-dark-blue mb-4 pl-[30px] md:pl-0">
                 Powerful tooling
               </h3>
               <p className="text-very-dark-grayish-blue">
