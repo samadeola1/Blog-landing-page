a6db92d77868665d680de1f191fb287df94e0347
Izzy-World <umezurikeisrael012@gmail.com>
2025-06-12 10:57:04 -0700

    update section 3

--- src/Home/Section3.jsx
+++ src/Home/Section3.jsx
@@ -5,26 +5,28 @@ const SimpleSection = () => {
   return (
     <section className="py-12 font-overpass">
       <div className="container mx-auto px-6">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="md:w-1/2">
+        <div className="flex flex-col lg:flex-row items-center">
+          {/* Image Container */}
+          <div className="lg:w-1/2">
             <img
               src={illustrationLaptopDesktop}
               alt="Laptop illustration"
-              className="hidden md:block w-full max-w-[700px] -left-40 relative"
+              className="hidden lg:block w-full max-w-[700px] lg:-left-40 relative"
             />
             <img
               src={illustrationLaptopMobile}
               alt="Laptop illustration"
-              className="md:hidden max-w-2xl block mx-auto mt-8"
+              className="lg:hidden w-full max-w-md block mx-auto mt-8"
             />
           </div>
 
-          <div className="md:w-1/2 md:pl-16 mt-16 md:mt-0 md:pr-40 ">
+          {/* Content Container */}
+          <div className="lg:w-1/2 lg:pl-16 mt-16 lg:mt-0 lg:pr-40 px-4 md:px-8">
             <div className="mb-12">
-              <h3 className="text-2xl font-bold text-center text-[#223D55] mb-4 md:pl-[30px]">
+              <h3 className="text-2xl md:text-3xl font-bold text-center lg:text-left text-[#223D55] mb-4">
                 Free, open, simple
               </h3>
-              <p className="text-[#7E8081] text-center">
+              <p className="text-[#7E8081] text-center lg:text-left md:text-lg">
                 Blogr is a free and open source application backed by a large
                 community of helpful developers. It supports features such as
                 code syntax highlighting, RSS feeds, social media integration,
@@ -35,10 +37,10 @@ const SimpleSection = () => {
             </div>
 
             <div>
-              <h3 className="text-2xl text-center font-bold text-[#223D55] mb-4 md:pl-[30px]">
+              <h3 className="text-2xl md:text-3xl font-bold text-center lg:text-left text-[#223D55] mb-4">
                 Powerful tooling
               </h3>
-              <p className="text-[#7E8081] text-center">
+              <p className="text-[#7E8081] text-center lg:text-left md:text-lg">
                 Batteries included. We built a simple and straightforward CLI
                 tool that makes customization and deployment a breeze, but
                 capable of producing even the most complicated sites.
